acf5510f1148257fe76ab0792c7bcf3b25142679
E-Sh4rk <esh4rk@gmail.com>
2019-01-21 03:10:32 +0100

    cleaning

--- typechecker.ml
+++ typechecker.ml
@@ -118,78 +118,30 @@ module IdSet = Set.Make(struct type t = identifier let compare = compare end)
 (** [eta_expanse source] makes sure that only functions are defined at
     toplevel and turns them into eta-long forms if needed. *)
 let eta_expanse : program_with_locations -> program_with_locations =
-   fun source ->
-      let eta_expanse_if_necessary (binding_loc, t_loc) =
-         let (_,typ) = binding_loc.Position.value in
-         match typ with
-         | TyArrow (typ, _) ->
-            let pos = t_loc.Position.position in
-            let term = t_loc.Position.value in
-            let new_t_loc =
-               begin match term with
-               | Lam _ -> t_loc
-               | t ->
-                  let new_id = Id "__x__" in
-                  let var_loc = { Position.position = pos; Position.value = Var new_id } in
-                  let app_loc = { Position.position = pos; Position.value = App (t_loc, var_loc) } in
-                  { Position.position = pos; Position.value = Lam ((new_id,typ), app_loc) }
-               end
-            in
-            (binding_loc, new_t_loc)
-            
-         | TyPair _ | TyConstant _ ->
-            type_error binding_loc.Position.position "Non-arrow toplevel definitions are not allowed!"
-      in
-      List.map eta_expanse_if_necessary source   
-
-(** [expanse_toplevel_non_arrow source] expanse every toplevel constants (definitions with a non-arrow type) so that they become functions. *)
-let expanse_toplevel_constants : program_with_locations -> program_with_locations =
-   fun source ->
-      (* Update the toplevel defitions *)
-      let expansed = ref IdSet.empty in
-      let expanse_if_necessary (binding_loc, t_loc) =
-         let (id,typ) = binding_loc.Position.value in
-         match typ with
-         | TyArrow _ -> (binding_loc, t_loc)
-         | TyPair _ | TyConstant _ ->
-            expansed := IdSet.add id !expansed ;
-            let new_t = Lam ((Id "_", TyConstant TyFloat), t_loc) in
-            let new_t_loc = { t_loc with Position.value = new_t } in
-            let new_binding = (id, TyArrow (TyConstant TyFloat,typ)) in
-            let new_binding_loc = { binding_loc with Position.value = new_binding } in
-            (new_binding_loc, new_t_loc)
-      in
-      let source = List.map expanse_if_necessary source in     
-      (* Update the call to these definitions *)
-      (* We can't directly use Source.map because it does not handle local environments *)
-      let rec expanse_calls_to (ids:IdSet.t) (term_loc:term' Position.located) : (term' Position.located) =
-         let pos = term_loc.position in
-         let term = term_loc.value in
-         let new_term = match term with
-            | App (t1, t2) -> App (expanse_calls_to ids t1, expanse_calls_to ids t2)
-            | Lam ((id,typ), t) ->
-               let ids = IdSet.remove id ids in
-               Lam ((id,typ), expanse_calls_to ids t)
-            | Pair (t1, t2) -> Pair (expanse_calls_to ids t1, expanse_calls_to ids t2)
-            | Fst t -> Fst (expanse_calls_to ids t)
-            | Snd t -> Snd (expanse_calls_to ids t)
-            | Var id ->
-               if IdSet.mem id ids
-               then
-                  let cst_loc = { Position.position = pos; Position.value = Literal (Float 0.) } in
-                  App (term_loc, cst_loc)
-               else Var id
-            | Literal l -> Literal l
-            | Primitive p -> Primitive p
+   let eta_expanse_if_necessary (binding_loc, t_loc) =
+      let (_,typ) = binding_loc.Position.value in
+      match typ with
+      | TyArrow (typ, _) ->
+         let pos = t_loc.Position.position in
+         let term = t_loc.Position.value in
+         let new_t_loc =
+            begin match term with
+            | Lam _ -> t_loc
+            | _ ->
+               let id = Id "__x__" in
+               let var_loc = { Position.position = pos; Position.value = Var id } in
+               let app_loc = { Position.position = pos; Position.value = App (t_loc, var_loc) } in
+               { Position.position = pos; Position.value = Lam ((id,typ), app_loc) }
+            end
          in
-         { Position.position = pos; Position.value = new_term }
-      in
-      let expanse_def (binding_loc, term_loc) =
-         (binding_loc, expanse_calls_to !expansed term_loc)
-      in
-      List.map expanse_def source
+         (binding_loc, new_t_loc)
+         
+      | TyPair _ | TyConstant _ ->
+         type_error binding_loc.Position.position "Non-arrow toplevel definitions are not allowed!"
+   in
+   List.map eta_expanse_if_necessary   
 
 let program : program_with_locations -> program_with_locations = fun source ->
   let xsource = check_program source in
   if !Options.typecheck_only then exit 0;
-  eta_expanse (expanse_toplevel_constants xsource)
+  eta_expanse xsource
