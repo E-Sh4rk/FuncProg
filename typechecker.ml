f508e71d0d928c45817e11b948cc7a097e50f786
E-Sh4rk <esh4rk@gmail.com>
2019-01-21 01:18:27 +0100

    Task2 check_program

--- typechecker.ml
+++ typechecker.ml
@@ -43,24 +43,71 @@ let string_of_type t =
 
 module IdMap = Map.Make(struct type t = identifier let compare = compare end)
 
-let check_term (env:typ IdMap.t) (term:term' Position.located) (t:typ) : unit =
+let rec type_of_term (env:typ IdMap.t) (term:term' Position.located) : typ =
    let pos = term.position in
    let term = term.value in
-   failwith "Student! This is your job!"
+   match term with
+   | Var id ->
+      if IdMap.mem id env then IdMap.find id env
+      else type_error pos "Identifier never declared."
+
+   | App (t1, t2) ->
+      begin match type_of_term env t1 with
+      | TyConstant _ -> type_error pos "Bad application: left term is a constant."
+      | TyArrow (typ1,typ1') ->
+         if type_of_term env t2 = typ1
+         then typ1'
+         else type_error pos "Bad application: wrong argument type."
+      | TyPair _ -> type_error pos "Bad application: left term is a pair."
+      end
+
+   | Lam ((id,typ), t) ->
+      let env = IdMap.add id typ env in
+      let typ' = type_of_term env t in
+      TyArrow (typ, typ')
+
+   | Pair (t1, t2) ->
+      let typ1 = type_of_term env t1 in
+      let typ2 = type_of_term env t2 in
+      TyPair (typ1, typ2)
+
+   | Fst t ->
+      begin match type_of_term env t with
+      | TyConstant _ -> type_error pos "Bad projection: term is a constant."
+      | TyArrow _ -> type_error pos "Bad projection: term is a function."
+      | TyPair (typ1,_) -> typ1
+      end
+
+   | Snd t ->
+      begin match type_of_term env t with
+      | TyConstant _ -> type_error pos "Bad projection: term is a constant."
+      | TyArrow _ -> type_error pos "Bad projection: term is a function."
+      | TyPair (_,typ2) -> typ2
+      end
+
+   | Literal lit -> TyConstant TyFloat
+
+   | Primitive prim ->
+      begin match prim with
+      | Sin | Cos | Exp | Inv | Neg -> TyArrow (TyConstant TyFloat, TyConstant TyFloat)
+      | Add | Mul -> (* According to the parser, it seems to be in curried form. *)
+         TyArrow (TyConstant TyFloat, TyArrow (TyConstant TyFloat, TyConstant TyFloat))
+      end
 
 (** [check_program source] returns [source] if it is well-typed or
    reports an error if it is not. *)
 let check_program (source : program_with_locations) : program_with_locations =
    (* Build the global environment *)
    let add_binding acc (binding_loc, _) =
-      let (id,t) = binding_loc.Position.value in
-      IdMap.add id t acc
+      let (id,typ) = binding_loc.Position.value in
+      IdMap.add id typ acc
    in
    let env = List.fold_left add_binding IdMap.empty source in
    (* Check all definitions *)
    let check_def (binding_loc, term_loc) =
-      let (_,t) = binding_loc.Position.value in
-      check_term env term_loc t
+      let (_,typ) = binding_loc.Position.value in
+      if type_of_term env term_loc <> typ
+      then type_error binding_loc.Position.position "Declared type and actual type mismatch!"
    in
    List.iter check_def source ; source
    
