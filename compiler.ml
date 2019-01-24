715d79068433d21ef47a5bfa1274f02f0e368fe9
E-Sh4rk <esh4rk@gmail.com>
2019-01-24 16:23:44 +0100

    compiler

--- compiler.ml
+++ compiler.ml
@@ -4,13 +4,13 @@ open Typechecker
 
 type ctx =
    | CtxEmpty
-   | CtxSingleton of identifier * typ * ok (* Var id, var type, var ok *)
+   | CtxSingleton of identifier * ok   (* Var id, var ok *)
    | CtxProd of ctx * ctx * ok         (* Left child, right child, ok for this node *)
 
 let ok_of_ctx ctx =
    match ctx with
    | CtxEmpty -> OkUnit
-   | CtxSingleton (_,_,ok) -> ok
+   | CtxSingleton (_,ok) -> ok
    | CtxProd (_,_,ok) -> ok
 
 let ok_of_primitive prim =
@@ -24,10 +24,27 @@ let rec ok_of_type typ =
    | TyArrow (typ1,typ2) -> OkArrow (ok_of_type typ1, ok_of_type typ2)
    | TyPair (typ1,typ2) -> OkPair (ok_of_type typ1, ok_of_type typ2)
 
-let ctx_of_binding (id, typ) = CtxSingleton (id, typ, ok_of_type typ)
+let ctx_of_binding (id, typ) = CtxSingleton (id, ok_of_type typ)
 
 let ctx_prod (ctx1:ctx) (ctx2:ctx) =
    CtxProd (ctx1, ctx2, OkPair(ok_of_ctx ctx1, ok_of_ctx ctx2))
+
+let rec extract_var_from_ctx (ctx:ctx) (id:identifier) : ((t * ok) option) =
+   match ctx with
+   | CtxEmpty -> None
+   | CtxSingleton (id',ok) when id'=id -> Some (Identity ok,ok)
+   | CtxSingleton _ -> None
+   | CtxProd (l,r,ok) ->
+      let (okl, okr) = match ok with OkPair(okl, okr) -> (okl,okr) | _ -> assert false in
+      let ext_l = extract_var_from_ctx l id
+      and ext_r = extract_var_from_ctx r id
+      in begin
+         match ext_l, ext_r with
+         | None, None -> None
+         | Some (tl,ok_var), None -> Some (compose ok okl ok_var tl (Exl (okl, okr)), ok_var)
+         | None, Some (tr,ok_var) -> Some (compose ok okr ok_var tr (Exr (okl, okr)), ok_var)
+         | _ -> assert false
+      end
 
 let rec compile_app (ctx:ctx) (u:term) (v:term) : (t * ok) =
    let ok_ctx = ok_of_ctx ctx in
@@ -37,6 +54,20 @@ let rec compile_app (ctx:ctx) (u:term) (v:term) : (t * ok) =
    let frk = fork ok_ctx oka okb ta tb in
    let apl = Apply (okb, oka') in
    (compose ok_ctx (OkPair (oka, okb)) oka' apl frk, oka')
+
+and compile_lam (ctx:ctx) (b:binding) (u:term) =
+   let ok_ctx = ok_of_ctx ctx in
+   let new_ctx = ctx_prod ctx (ctx_of_binding b) in
+   let ok_var = ok_of_type (snd b) in
+   let (t, ok) = compile_term new_ctx u (* t : Γ × A -> B *) in
+   (curry ok_ctx ok_var ok t, OkArrow (ok_var, ok))
+
+and compile_var (ctx:ctx) (id:identifier) =
+   match extract_var_from_ctx ctx id with
+   | Some res -> res
+   | None ->
+      (* Global function *)
+      failwith "Student! This is your job!"
 
 and compile_const (ctx:ctx) (c:literal) : (t * ok) =
    let ok_ctx = ok_of_ctx ctx in
