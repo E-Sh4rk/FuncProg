46588333cb3eee0cc3ed5d2357c2ff99d02ec1a6
E-Sh4rk <esh4rk@gmail.com>
2019-01-25 17:43:16 +0100

    simplifier

--- simplifier.ml
+++ simplifier.ml
@@ -1,5 +1,18 @@
 open Target
 
+type cat_term =
+  | Identity of ok
+  | Curry of ok * ok * ok * cat_term
+  | UnCurry of ok * ok * ok * cat_term
+  | Apply of ok * ok
+  | Fork of ok * ok * ok * cat_term * cat_term
+  | Exl of ok * ok
+  | Exr of ok * ok
+  | UnitArrow of ok * cat_term
+  | It of ok
+  | Compose of (ok * cat_term * ok) list
+  | Literal of Source.literal
+  | Primitive of Source.primitive
 
 (** [rewrite defs] applies category laws to remove [apply] and [curry]
     from the compiled programs. *)
--- target.ml
+++ target.ml
@@ -29,5 +29,8 @@ let fork oka okb okc a1 a2 =
 let curry oka okb okc a =
   App ((Curry (oka, okb, okc)), a)
 
+let uncurry oka okb okc a =
+  App ((UnCurry (oka, okb, okc)), a)
+
 let unit_arrow ok t =
   App (UnitArrow ok, t)
