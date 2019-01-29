e7f32d844cbcc0ecb15f0e0772131265388aed16
E-Sh4rk <esh4rk@gmail.com>
2019-01-29 20:22:21 +0100

    step epsilon = max error

--- tiny_mlp_test.ml
+++ tiny_mlp_test.ml
@@ -28,21 +28,23 @@ let print_net (w1, w2) = Printf.printf "%f %f\n" w1 w2
 (** [train training_set] returns a neural network trained for the
     [training_set]. *)
 let train : training_set -> net = fun tset ->
-  let eps = DiffCat.epsilon (*1.*) in
+  let eps = (* DiffCat.epsilon *) 1. in
   let res = ref (make_net (0.) (0.)) in
-  while eval_net !res tset >= acceptable_error
+  let last_error = ref (eval_net !res tset) in
+  while !last_error >= acceptable_error
   do
     let train_on tset =
       let (input,exp) = tset in
       let entry = ((input, !res), exp) in
-      let entry_eps_1 = (((0.,0.),(eps, 0.)),0.) in
-      let entry_eps_2 = (((0.,0.),(0., eps)),0.) in
+      let entry_eps_1 = (((0.,0.),(!last_error *. eps, 0.)),0.) in
+      let entry_eps_2 = (((0.,0.),(0., !last_error *. eps)),0.) in
       let diff1 = DiffCat.epsilon_dapply Diff.error entry entry_eps_1 in
       let diff2 = DiffCat.epsilon_dapply Diff.error entry entry_eps_2 in
       res := update_net (!res) (-.diff1) (-.diff2)
     
     in List.iter train_on tset ;
-    (* print_net !res ; Printf.printf "%f\n" (eval_net !res tset) *)
+    last_error := eval_net !res tset ;
+    (*print_net !res ; Printf.printf "%f\n" (!last_error) ; *)
   done ;
   !res
 
